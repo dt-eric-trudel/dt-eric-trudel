7a663e2025b40399682dd918aa698a401a93f68b
Éric Trudel <97742650+dt-eric-trudel@users.noreply.github.com>
2024-08-19 15:37:12 -0400

    Update macos_legal_notice_on_user_logon.sh
    updated

--- macos_legal_notice_on_user_logon.sh
+++ macos_legal_notice_on_user_logon.sh
@@ -1,5 +1,7 @@
+username="$(stat -f%Su /dev/console)"
+realname="$(dscl . -read /Users/$username RealName | cut -d: -f2 | sed -e 's/^[ \t]*//' | grep -v "^$")"
 loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
-LastLog=$(last -n 5 $loggedInUser | grep 'ttys000'| grep --line-buffered 2)
+LastLog=$(last -n 5 $loggedInUser | grep 'ttys000'| grep --line-buffered 2 | cut -d' ' -f30,31,32,33)
 osascript -e 'tell app "System Events" to display dialog "This system is restricted to DiliTrust authorized users. Unauthorized access is a violation of company policy and the law.
 
-Last login data and time : '$LastLog'"'
+Last login data and time : '$realname' - '$LastLog'"'
